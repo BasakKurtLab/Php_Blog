b1c25ffda22b232e32dbbce4d10423154b7603ff
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-11-18 00:13:38 +0100

    kategorie added

--- blog.php
+++ blog.php
@@ -1,14 +1,24 @@
 <?php
+    include("verbinden.php");
     include("header.php");
 ?>
 
 
 <main>
     <h1>Willkommen in Blog</h1>
+
     Kategorien:
     <ul>
         <?php
-            
+            $anfrage = "SELECT * FROM kategorien";
+            $result = $verbindung->query($anfrage);
+
+            while($row = $result->fetch_assoc())
+            {
+                ?>
+                <li><a href="category.php?id=<?= $row["id"] ?>"><?= $row["name"] ?></a></li>
+                <?php
+            }
         ?>
     </ul>
 </main>
@@ -19,4 +29,5 @@
     include("sidebar.php");
 
     include("footer.php");
-?>
+    include("close.php");
+?>>
