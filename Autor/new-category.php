2085adbf20b3934c176e98ab9271d173a00c1442
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-11-17 01:35:37 +0100

    admin panel created

---
+++ Autor/new-category.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Neue Kategorie erstellen:
+
+    <form method="POST" action="request.php">
+        <input type="text" placeholder="Name" name="kat_name">
+        <input type="submit" name="new_category" value="Erstellen">
+    </form>
+</body>
+</html>
