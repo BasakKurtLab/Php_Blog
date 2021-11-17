dc6c992e02506698a25dfdae77af95920f4e58f9
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-11-17 01:31:43 +0100

    main created

---
+++ blog.php
@@ -0,0 +1,22 @@
+<?php
+    include("header.php");
+?>
+
+
+<main>
+    <h1>Willkommen in Blog</h1>
+    Kategorien:
+    <ul>
+        <?php
+            
+        ?>
+    </ul>
+</main>
+
+
+
+<?php
+    include("sidebar.php");
+
+    include("footer.php");
+?>
