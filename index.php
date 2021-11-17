540c83bde5926efe07f825783c57d154a6e34f67
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-11-18 00:17:40 +0100

    index added

--- index.php
+++ index.php
@@ -3,8 +3,25 @@
 ?>
 
     <main>
-        <h1>Willkommen in Startseite</h1>
+        <section class="hero">
+            <h1>Herzlich Willkommen</h1>
+        </section>
+        
+        <section class="einleitung">
+            <h3>Wer bin ich?</h3>
+            <p>
+                <?= "dynamischer Text von Datenbank" ?>
+            </p>
+            <a href="uber-mich.php">Weiterlesen</a>
+        </section>
 
+        <section class="beratung">
+            <h3>Kostenlose Beratung</h3>
+            <p>
+                ...
+            </p>
+            <a href="kontakt.php">Jetzt kostenlos beraten lassen!</a>
+        </section>
     </main>
 
     
