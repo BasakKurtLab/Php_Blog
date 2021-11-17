a5b120c65006950085f84fd16c5bf1af97dae89a
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-11-17 01:37:09 +0100

    request created

---
+++ Autor/request.php
@@ -0,0 +1,16 @@
+<?php
+
+    if(isset($_POST["new_category"]))
+    {
+        exit("KATEGORIE ERSTELLT!");
+    }
+    else if(isset($_POST["new_post"]))
+    {
+        exit("BEITRAG ERSTELLT!");
+    }
+
+
+
+
+
+?>
