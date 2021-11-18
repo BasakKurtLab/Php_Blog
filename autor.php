b184d8964be517c1a85789ffc614a0d6db0ed2e5
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-11-18 01:26:30 +0100

    autor created

---
+++ autor.php
@@ -0,0 +1,67 @@
+<?php
+    include("verbinden.php");
+    include("header.php");
+
+    if(!empty($_GET["id"]))
+    {
+        $autor_id = $_GET["id"];
+    }
+?>
+
+<main>
+    <?php
+        if(!isset($autor_id))
+        {
+            echo("Autor nicht gefunden.");
+        }
+        else
+        {
+            $anfrage = "SELECT * FROM autoren WHERE id=" . $autor_id;
+            $result = $verbindung->query($anfrage);
+
+            if($result->num_rows == 0)
+            {
+                echo("Autor nicht gefunden.");
+            }
+            else
+            {
+                $autor_row = $result->fetch_assoc(); // Autor row
+
+                $anfrage = "SELECT * FROM posts WHERE autor=" . $autor_id;
+                $result = $verbindung->query($anfrage);
+
+                while($post_row = $result->fetch_assoc()); // post row
+
+                {
+
+                    
+                     ?>
+
+                    <span>Autor: <?= $autor_row["name"] ?></span>
+
+                    <hr>
+
+                    <a href="post.php?id=<?= $post_row["id"] ?>"><?= $post_row["name"] ?></a><br>
+
+                    
+
+                
+
+                    
+
+                <?php
+                }
+
+               
+            }
+        }
+        ?>
+</main>
+
+
+<?php
+    include("sidebar.php");
+
+    include("footer.php");
+    include("close.php");
+?>
